57a0a9042ebf12fe1fcf41cc694ddf4411f5329c
Vani1997 <sirasapallivani1997@gmail.com>
2023-06-02 01:10:24 -0500

    new added file

---
+++ day1/Hello.java
@@ -0,0 +1,24 @@
+package day1;
+
+import day2.Student;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        Student vbudi = new Student();
+        System.out.println("what is your name ="+ vbudi.getName());
+
+        vbudi.setName("vaani");
+
+        System.out.println("what is your name ="+ vbudi.getName());
+
+
+
+
+
+    }
+}
