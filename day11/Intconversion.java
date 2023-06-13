06b1bc2e9a4338228be5e3763192e97ca1816f7b
Vani1997 <sirasapallivani1997@gmail.com>
2023-06-13 15:11:56 -0500

    new added file

---
+++ day11/Intconversion.java
@@ -0,0 +1,22 @@
+package day11;
+
+public class Intconversion {
+    static String s;
+    public Intconversion(String s){
+        this.s=s;
+    }
+     public  int test(){
+        return Integer.parseInt(s);
+    }
+    public static void main(String[] arg){
+        try{
+         Intconversion i=new Intconversion("23");
+         System.out.println("converted string "+ s +" to" +i.test());
+        }
+        catch(NumberFormatException e){
+            System.out.println("cannot convert" + s+"to int");
+        }
+
+    }
+
+}
